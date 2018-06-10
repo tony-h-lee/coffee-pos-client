dd1d12ca73c6595ef8eac10e887d543051d2d950
tony-h-lee <tonyhjlee@gmail.com>
2018-06-10 18:34:01 -0400

    added views section

---
+++ src/views/HomePage/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style lang="scss" scoped>
+  .grid {
+    display: flex;
+  }
+</style>
